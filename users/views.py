427df6d0d9490ce0cbbc6c2a80bc6060e8f965ff
Kadirov Andrei <kadyrov_andrei@mail.ru>
2024-03-05 13:43:32 +0300

    Login and Logout

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from . import forms
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -10,11 +10,13 @@ def register(request):
         form = forms.UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            # cleaned data is a dictionary
             username = form.cleaned_data.get('username')
-            messages.success(
-                request, f"{username}, you're account is created!")
-            return redirect('recipes-home')
+            messages.success(request, f"{username}, you're account is created, please login")
+            return redirect('user-login')
     else:
         form = forms.UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required()
+def profile(request):
+  return render(request, 'users/profile.html')
