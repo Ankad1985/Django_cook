fc21cd2bd2bd287f3df59b8c0af1bcf5c474827e
Kadirov Andrei <kadyrov_andrei@mail.ru>
2024-03-05 15:27:13 +0300

    add Recipes CRUD

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from . import models
 
@@ -8,6 +10,22 @@ def home(request):
     'recipes': recipes
   }
   return render(request, 'recipes/home.html', context)
+
+class RecipeListView(ListView):
+  model = models.Recipe
+  template_name = 'recipes/home.html'
+  context_object_name = 'recipes'
+
+class RecipeDetailView(DetailView):
+  model = models.Recipe
+
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+  model = models.Recipe
+  fields = ['title', 'description']  
+
+  def form_valid(self, form):
+    form.instance = self.request.user
+    return super().form_valid(form)
 
 def about(request):
   return render(request, 'recipes/about.html', {'title': 'about page'})
